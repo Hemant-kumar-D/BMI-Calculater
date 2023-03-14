932398a5491b4c6b4221c48328f50e6f5453ef34
Hemant kumar <hemantkumar42852@gmail.com>
2023-03-14 16:07:54 +0530

    updatemenubar commit

--- app/src/main/java/com/example/assigement_01/MainActivity.kt
+++ app/src/main/java/com/example/assigement_01/MainActivity.kt
@@ -1,23 +1,34 @@
 package com.example.assigement_01
 
 
+import android.content.Intent
+import android.content.pm.PackageManager.PERMISSION_GRANTED
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 //import android.view.View.OnClickListener
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlin.math.round
+
 import kotlin.math.roundToInt
 
 
+
+
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var data:String?=null
 
         var isclear = false
+
 
         btn_1.setOnClickListener {
             if (!edit_1.text.toString().equals("") && edit_2.text.toString().equals("")) {
@@ -33,34 +44,52 @@ class MainActivity : AppCompatActivity() {
                 if (!isclear) {
                     Toast.makeText(this, "clear karo", Toast.LENGTH_LONG).show()
                     isclear = true
+                    edit_2.isEnabled = false
+                    edit_1.isEnabled = false
+
                     btn_1.text = "CLEAR"
                     val wi = edit_1.text.toString().toDouble()
                     val le = edit_2.text.toString().toDouble()
                     val hi = le / 100
                     val answer = wi / (hi * hi)
                     val total=(answer*100).roundToInt()/100.0
+                    if(le<=0.0){
+                        Toast.makeText(this,"Please fill valid value",Toast.LENGTH_LONG).show()
+                       
+                    }
+                     else {
+                        //textview_05.setText( "Your BMI Value =" + total.toString())
+                        if (total > 25) {
+                            Toast.makeText(this, "You are overweight", Toast.LENGTH_LONG).show()
+                           textview_05.setText("You are Overweight \n Your BMI Value= ${total.toString()}")
+                        } else if (total < 18) {
+                            Toast.makeText(this, "You are underweight", Toast.LENGTH_LONG).show()
+                            textview_05.setText("You are Underweight \n Your BMI Value= ${total.toString()}")
+                        }
+                        else if(total>30){
+                            Toast.makeText(this, "You are OBESITY", Toast.LENGTH_LONG).show()
+                            textview_05.setText("You are OBERSITY \n Your BMI Value= ${total.toString()}")
 
-                    textview_1.text = "Your BMI Value ="+total.toString()
-                    if (total > 25) {
-                        Toast.makeText(this, "You are overweight", Toast.LENGTH_LONG).show()
-                        text_2.text = "You are Overweight"
-                    } else if (total < 18) {
-                        Toast.makeText(this, "You are underweight", Toast.LENGTH_LONG).show()
-                        text_2.text = "You are underweight"
-                    } else {
-                        Toast.makeText(this, "You are fit", Toast.LENGTH_LONG).show()
-                        text_2.text = "You are fit"
+                        }
+                        else {
+                            Toast.makeText(this, "You are fit", Toast.LENGTH_LONG).show()
+                            textview_05.setText("You are Fit \n Your BMI Value=${ total.toString()}")
+                        }
+
                     }
                 } else if (isclear) {
                     btn_1.text = "Calculate BMi"
-                    edit_1.text.clear()
-                    edit_2.text.clear()
-                    text_2.setText("Stetus")
-                    textview_1.setText("BMI VALUE")
+                    edit_1.text?.clear()
+                    edit_2.text?.clear()
+                   textview_05.setText("")
+                    edit_2.isEnabled=true
+                    edit_1.isEnabled=true
+
                     isclear = false
 
 
                 }
+
 
 
             } else {
@@ -74,18 +103,20 @@ class MainActivity : AppCompatActivity() {
         inflater.inflate(R.menu.menu_click, menu)
         return super.onCreateOptionsMenu(menu)
     }
-
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
         when (item.itemId) {
             R.id.Ab_bmi -> {
-                Toast.makeText(this,"About Bmi",Toast.LENGTH_LONG).show()
+                val inten=Intent(this,BMIChart::class.java)
+                startActivity(inten)
                 return true
 
 
             }
             R.id.bmi_chart -> {
 
-                Toast.makeText(this,"BMI Chart",Toast.LENGTH_LONG).show()
+                val intent = Intent(this, MainActivity2::class.java)
+                startActivity(intent)
                 return true
 
             }
@@ -97,6 +128,33 @@ class MainActivity : AppCompatActivity() {
                 return true
 
             }
+            R.id.call_menu  ->{
+                if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.CALL_PHONE) == PERMISSION_GRANTED) {
+                    val intent = Intent(Intent.ACTION_CALL)
+                    intent.data = Uri.parse("tel:6396299156")
+                    startActivity(intent)
+                } else {
+                    ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.CALL_PHONE), 1001
+                    )
+                }
+            }
+            R.id.dial ->{
+                val intent=Intent(Intent.ACTION_DIAL,Uri.parse("tel:6396299156"))
+                startActivity(intent)
+            }
+            R.id.what_bmi ->{
+                val intent=Intent(Intent.ACTION_VIEW,Uri.parse("https://www.youtube.com/watch?v=X8G1YkurALI"))
+                startActivity(intent)
+            }
+            R.id.email_menu ->{
+                val intent=Intent(Intent.ACTION_SENDTO).apply {
+                    data=Uri.parse("mailto:")
+                    putExtra(Intent.EXTRA_EMAIL, arrayOf("hemantkumar42852@gmail.com"))
+                    putExtra(Intent.EXTRA_SUBJECT,"Hello  Friends   I hope you are well-in good ")
+                }
+                startActivity(intent)
+
+            }
 
 
         }
@@ -105,7 +163,7 @@ class MainActivity : AppCompatActivity() {
 
 
     override fun onBackPressed() {
-       Toast.makeText(this,"not work back button",Toast.LENGTH_LONG).show():
+       Toast.makeText(this,"Please go to option menu and press tha exit option",Toast.LENGTH_LONG).show()
     }
 }
 
